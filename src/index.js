61f1ee6fd65dff1823de0d70546709f14d867cfc
Artsemi <stojanka25@gmail.com>
2022-01-08 00:13:50 +0300

    feat add human dictionary

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const decimals = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
 module.exports = function toReadable (number) {
   
 }
