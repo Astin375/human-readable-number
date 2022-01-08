14a8173f5adf307bc17a5ac6294b9b4d21594f2a
Artsemi <stojanka25@gmail.com>
2022-01-08 14:01:43 +0300

    feat: pass tests up to 99

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-const decimals = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
 module.exports = function toReadable (number) {
-  
+    const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const decimals = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    switch (number.toString().length){
+        case 1:
+            return digits[number];
+        case 2:
+            if(number < 20){
+                return teens[number - 10];
+            } else if(number % 10 === 0){
+                return decimals[number / 10];
+            } else{
+                return decimals[Math.floor(number / 10)] + ' ' + digits[number % 10];
+            }
+        default:
+            return 'Error';
+
+    }
 }
